b49b9856f25d3f06fff23a24f333c69fd1a58fcc
maxFischer42 <42ducksable1@gmail.com>
2019-01-07 06:00:28 -0800

    Battle UI Layout fix

---
+++ Assets/EnemyResources/Arts/EnemyArts.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyArts : MonoBehaviour {
+
+    private EnemyMovement enMov;
+
+
+	// Use this for initialization
+	void Start () {
+        enMov = GetComponent<EnemyMovement>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

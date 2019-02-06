35a5a4b6a4223b0181faaf2886995a87da5bc5b3
Max Fischer <42ducksable1@gmail.com>
2019-02-06 15:20:56 -0800

    Dialouge System Added
    A new system for displaying and writing out dialouge for a future cutscene system.

---
+++ Assets/Dialouge.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialouge : MonoBehaviour {
+
+    public DialougeObject[] Conversation;
+    public DialougeObject currentObject;
+    public Text textObject;
+    private float typeSpeed = 0.1f;
+    private string targetString;
+    private int currentCharacter;
+    public string currentString;
+    private float timer;
+    int current;
+
+    private void Start()
+    {
+        ChangeDialouge(0);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(targetString == currentString)
+            {
+                ChangeDialouge(current + 1);
+            }
+            else
+            {
+                currentString = targetString;
+            }
+        }
+        textObject.text = currentString;
+		if(targetString != currentString && timer >= (typeSpeed / Time.timeScale))
+        {
+            currentString = targetString.Substring(0,currentCharacter);
+            currentCharacter++;
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+
+    }
+
+    void ChangeDialouge(int select)
+    {
+        currentObject = Conversation[select];
+        textObject.color = currentObject.Color;
+        textObject.lineSpacing = currentObject.LineSpacing;
+        textObject.fontSize = currentObject.Size;
+        targetString = currentObject.Dialouge;
+        typeSpeed = currentObject.TypeSpeed;
+        currentCharacter = 0;
+    }
+}
---
+++ Assets/DialougeObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DialougeObject : ScriptableObject
+{
+
+    public string Dialouge;
+    public Color Color = Color.white;
+    public int Size = 60;
+    public int LineSpacing = 1;
+    public float TypeSpeed = 0.1f;
+	
+}
